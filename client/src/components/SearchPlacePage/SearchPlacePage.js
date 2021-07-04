92a75d7cae5bd697dd94c8e03049b14d57c9747a
seungwonleee <seungwon.code@gmail.com>
2021-07-04 18:53:44 +0900

    영화관 검색바 css 수정

--- client/src/components/SearchPlacePage/SearchPlacePage.js
+++ client/src/components/SearchPlacePage/SearchPlacePage.js
@@ -1,9 +1,38 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import KakaoMap from "./KakaoMap";
+// Material UI Imports
 import SearchIcon from "@material-ui/icons/Search";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { withStyles } from "@material-ui/core/styles";
 
-const Container = styled.div`
+const SearchPlace = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "#A5292A",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#A5292A",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#A5292A",
+      },
+      "&:hover fieldset": {
+        borderColor: "#A5292A",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#A5292A",
+      },
+    },
+    width: "60%",
+    height: "3rem",
+  },
+})(TextField);
+
+const PageContainer = styled.div`
   background: #2a2d2e;
 `;
 
@@ -11,20 +40,6 @@ const Form = styled.form`
   display: flex;
   justify-content: center;
   padding: 3rem;
-`;
-
-const Input = styled.input`
-  width: 60%;
-  height: 3rem;
-  font-size: ${(props) => props.theme.fontSizes.base};
-  border-radius: 4px;
-  border: 1px solid #ccc;
-  padding: 1rem;
-`;
-
-const Button = styled.button`
-  border-radius: 4px;
-  border: 1px solid #ccc;
 `;
 
 const MapContainer = styled.div`
@@ -47,21 +62,37 @@ const SearchPlacePage = () => {
   };
 
   return (
-    <Container>
+    <PageContainer>
       <Form className="inputForm" onSubmit={handleSubmit}>
-        <Input
-          placeholder="장소를 검색하세요."
+        <SearchPlace
+          autoFocus
+          placeholder="지역명 + 영화관으로 검색하세요."
           onChange={onChange}
           value={inputText}
+          InputProps={
+            ({
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton type="submit">
+                    <SearchIcon fontSize="large" />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            },
+            {
+              style: {
+                fontSize: "1.6rem",
+                color: "white",
+                fontFamily: "sans-serif",
+              },
+            })
+          }
         />
-        <Button type="submit">
-          <SearchIcon fontSize="large" />
-        </Button>
       </Form>
       <MapContainer>
         <KakaoMap searchPlace={place} />
       </MapContainer>
-    </Container>
+    </PageContainer>
   );
 };
 
